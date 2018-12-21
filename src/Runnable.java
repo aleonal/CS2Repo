4df35a80c6206b2b8b4ecde1bf6fc15056275d1b
aleonal <thelifeofantoine@outlook.com>
2018-12-21 16:39:01 -0700

    Updated first example.

--- src/Runnable.java
+++ src/Runnable.java
@@ -2,6 +2,6 @@ public class Runnable {
   public static void main(String[] args) {
     //Example 1 - Given an nxn int array, print all the elements
     int[][] nxnarray = new int[5][5];
-    printnxn(nxnarray);
+    arrays.printnxn(nxnarray);
   }
 }
---
+++ src/arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+  public static void printnxn(int[][] array) {
+    for(int i = 0; i < array.length; i++) {
+      for(int j = 0; j < array.length; j++)
+        System.out.print(array[i][j] + " ");
+
+      System.out.println();
+    }
+  }
+}
