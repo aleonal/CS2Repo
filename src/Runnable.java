48b5e90b72381111f1dad0132a96816645a70e6e
aleonal <thelifeofantoine@outlook.com>
2018-12-21 16:30:51 -0700

    Completed Example 1

---
+++ src/Runnable.java
@@ -0,0 +1,7 @@
+public class Runnable {
+  public static void main(String[] args) {
+    //Example 1 - Given an nxn int array, print all the elements
+    int[][] nxnarray = new int[5][5];
+    printnxn(nxnarray);
+  }
+}
