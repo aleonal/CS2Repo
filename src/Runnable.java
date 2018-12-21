de1fa84a6273f70fa8f4f994c37ef3cd036cd115
aleonal <thelifeofantoine@outlook.com>
2018-12-21 16:45:12 -0700

    Completed Example 2

--- src/Runnable.java
+++ src/Runnable.java
@@ -3,5 +3,12 @@ public class Runnable {
     //Example 1 - Given an nxn int array, print all the elements
     int[][] nxnarray = new int[5][5];
     arrays.printnxn(nxnarray);
+
+    //Example 2 - given an nx? int array (ragged array), print all elements
+    int[][] raggedarray = new int[3][];
+    raggedarray[0] = new int[3];
+    raggedarray[1] = new int[5];
+    raggedarray[2] = new int[10];
+    arrays.printragged(raggedarray);
   }
 }
--- src/arrays.java
+++ src/arrays.java
@@ -6,5 +6,16 @@ public class arrays {
 
       System.out.println();
     }
+    System.out.println();
+  }
+
+  public static void printragged(int[][] array) {
+    for(int i = 0; i < array.length; i++) {
+      for(int j = 0; j < array[i].length; j++)
+        System.out.print(array[i][j] + " ");
+
+      System.out.println();
+    }
+    System.out.println();
   }
 }
